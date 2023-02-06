c77a41e2bd69f7a8af160729c36c0a2d3bb2531d
Umar Sani Muhammad <39451340+Umarsmuhammed@users.noreply.github.com>
2023-02-06 10:11:10 +0100

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,44 @@
+#Exercise Level 2
+#Question 1
+# my python version is 3.10.8
+#Question 2
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 % 4)             # modulus(%)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 // 4)            # Floor division operator(//)
+#QUESTION 3
+print('My name is Umar')
+print('My family name is Sani')
+print('Iam from Nigeria')
+print('I am enjoying 30 days of python')
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+#Exercise Level 3
+#Question 1
+print(type(80))          # Int
+print(type(36.72))        # Float
+print(type(5 + 2j))      # Complex number
+print(type('Ammar'))  # String
+print(type(True))     # Boolean
+print(type([2,4,6,8]))   # List
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type({1.0,1.5,2.0,2.5,3.0}))    # Set
+print(type({'name':'Ummulkhair'})) # Dictionary
+
+#Question 2
+#Euclidean distance
+X1, X2, Y1, Y2 = 10,2,8,3
+D= (X1-X2)**2+(Y1-Y2)**2
+Euclidean_distance = D*D
+print(Euclidean_distance)
+
