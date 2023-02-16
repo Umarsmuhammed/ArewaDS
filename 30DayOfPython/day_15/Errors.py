ae87554fddb1828c07082d9d51ef227997bfb0f0
umarsmuhammed <umarsmuhammed@gmail.com>
2023-02-16 15:47:36 +0100

    Update Errors.py

---
+++ 30DayOfPython/day_15/Errors.py
@@ -0,0 +1,15 @@
+# Day 15 - 30DaysOfPython Exercises 
+
+#I was able to run all the examples in day 15 in the python shell at the end i learned 
+# the following type of errors:
+# SyntaxError
+# NameError
+# IndexError
+# ModuleNotFoundError
+# AttributeError
+# KeyError
+# TypeError
+# ImportError
+# ValueError
+# ZeroDivisionError
+# At the end i was able to detect and corrected all the errors
