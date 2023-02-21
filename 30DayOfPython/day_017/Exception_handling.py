1dfa4b1824071066c842c26ce2e32a86c7c07829
umarsmuhammed <umarsmuhammed@gmail.com>
2023-02-21 23:29:33 +0100

    Exception_handling

---
+++ 30DayOfPython/day_017/Exception_handling.py
@@ -0,0 +1,8 @@
+# Day 17: 30 days of Python programming: Exception handling
+'''Packing and unpacking in Python, Spreading, Enumerate and Zip '''
+
+#Exercise Day 17
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries,es,ru = names
+print(f'The Nordic countries include: {nordic_countries} ')
+print(f'es: {es}, ru: {ru}')
